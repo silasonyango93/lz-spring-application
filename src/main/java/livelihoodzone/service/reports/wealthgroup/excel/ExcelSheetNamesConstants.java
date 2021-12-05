78e66ee6c7634759b0161dc0250b884f08869885
Silas Onyango <silas.onyango93@gmail.com>
2021-12-05 17:44:54 +0300

    complete crop production excel export

--- src/main/java/livelihoodzone/service/reports/wealthgroup/excel/ExcelSheetNamesConstants.java
+++ src/main/java/livelihoodzone/service/reports/wealthgroup/excel/ExcelSheetNamesConstants.java
@@ -3,4 +3,5 @@ package livelihoodzone.service.reports.wealthgroup.excel;
 public class ExcelSheetNamesConstants {
     public static final String MAIN_INCOME_SOURCES_EXCEL_SHEET_NAME = "Main Sources of Income and Food";
     public static final String FOOD_CONSUMPTION_PERCENTAGES_EXCEL_SHEET_NAME = "Food Consumption Percentage";
+    public static final String CROP_CONTRIBUTION = "Crop Contribution";
 }
--- src/main/java/livelihoodzone/service/reports/wealthgroup/excel/WealthGroupExcelService.java
+++ src/main/java/livelihoodzone/service/reports/wealthgroup/excel/WealthGroupExcelService.java
@@ -20,13 +20,19 @@ public class WealthGroupExcelService {
     @Autowired
     FoodConsumptionPercentageExcelService foodConsumptionPercentageExcelService;
 
+    @Autowired
+    WgCropProductionExcelService wgCropProductionExcelService;
+
     private XSSFWorkbook workbook;
 
     public void processData(int countyId, int wealthGroupId) {
         workbook.createSheet(MAIN_INCOME_SOURCES_EXCEL_SHEET_NAME);
         workbook.createSheet(FOOD_CONSUMPTION_PERCENTAGES_EXCEL_SHEET_NAME);
+        workbook.createSheet(CROP_CONTRIBUTION);
+
         workbook = mainSourcesOfFoodAndIncomeExcelService.processData(countyId,wealthGroupId,workbook);
         workbook = foodConsumptionPercentageExcelService.processData(countyId,wealthGroupId,workbook);
+        workbook = wgCropProductionExcelService.processData(countyId,wealthGroupId,workbook);
     }
 
 
