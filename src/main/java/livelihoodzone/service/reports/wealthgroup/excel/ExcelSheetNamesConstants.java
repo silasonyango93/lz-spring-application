3109be57a51581e26760c5d63a906347ab782e72
Silas Onyango <silas.onyango93@gmail.com>
2021-12-05 19:56:24 +0300

    complete livestock contribution excel export

--- src/main/java/livelihoodzone/service/reports/wealthgroup/excel/ExcelSheetNamesConstants.java
+++ src/main/java/livelihoodzone/service/reports/wealthgroup/excel/ExcelSheetNamesConstants.java
@@ -5,4 +5,5 @@ public class ExcelSheetNamesConstants {
     public static final String FOOD_CONSUMPTION_PERCENTAGES_EXCEL_SHEET_NAME = "Food Consumption Percentage";
     public static final String CROP_CONTRIBUTION = "Crop Contribution";
     public static final String LIVESTOCK_OWNERSHIP = "Livestock Ownership";
+    public static final String LIVESTOCK_CONTRIBUTION = "Livestock Contribution";
 }
--- src/main/java/livelihoodzone/service/reports/wealthgroup/excel/WealthGroupExcelService.java
+++ src/main/java/livelihoodzone/service/reports/wealthgroup/excel/WealthGroupExcelService.java
@@ -26,6 +26,9 @@ public class WealthGroupExcelService {
     @Autowired
     LivestockOwnershipExcelService livestockOwnershipExcelService;
 
+    @Autowired
+    LivestockContributionExcelService livestockContributionExcelService;
+
     private XSSFWorkbook workbook;
 
     public void processData(int countyId, int wealthGroupId) {
@@ -33,11 +36,13 @@ public class WealthGroupExcelService {
         workbook.createSheet(FOOD_CONSUMPTION_PERCENTAGES_EXCEL_SHEET_NAME);
         workbook.createSheet(CROP_CONTRIBUTION);
         workbook.createSheet(LIVESTOCK_OWNERSHIP);
+        workbook.createSheet(LIVESTOCK_CONTRIBUTION);
 
         workbook = mainSourcesOfFoodAndIncomeExcelService.processData(countyId,wealthGroupId,workbook);
         workbook = foodConsumptionPercentageExcelService.processData(countyId,wealthGroupId,workbook);
         workbook = wgCropProductionExcelService.processData(countyId,wealthGroupId,workbook);
         workbook = livestockOwnershipExcelService.processData(countyId,wealthGroupId,workbook);
+        workbook = livestockContributionExcelService.processData(countyId,wealthGroupId,workbook);
     }
 
 
